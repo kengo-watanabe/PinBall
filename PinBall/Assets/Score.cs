947116bb318d8a4c98d150e79606f90b589c989b
渡辺　健吾 <kengo978@yahoo.co.jp>
2017-08-07 21:39:29 +0900

    PinBallプロジェクト初めてのコミット
    通常課題まで

---
+++ PinBall/Assets/Score.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    //スコアを表示するテキスト
+    private GameObject ScoreText;
+    //初期スコアを設定
+    int TotalScore = 0;
+    //タグによっての点数を決める
+    //SmallStarTagのスコア
+    int SmallStarScore = 10;
+    //LargeStarTagのスコア
+    int LargeStarScore = 20;
+    //SmallCloudTagのスコア
+    int SmallCloudScore = 30;
+    //LargeCloudTagのスコア
+    int LargeCloudScore = 50;
+
+	// Use this for initialization
+	void Start () {
+       
+        //シーン中のScoreTextオブジェクトを取得
+        this.ScoreText = GameObject.Find("ScoreText");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {     
+
+        //ScoreTextにスコアを表示
+        this.ScoreText.GetComponent<UnityEngine.UI.Text>().text = "Score" + TotalScore;
+		
+	}
+    //衝突時に呼ばれる関数
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            TotalScore += SmallStarScore; 
+        }else if(collision.gameObject.tag == "LargeStarTag")
+        {
+            TotalScore += LargeStarScore;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            TotalScore += SmallCloudScore;
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            TotalScore += LargeCloudScore;
+        }
+
+
+
+    }
+}
+
---
+++ PinBall/Assets/TouchScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScreen : MonoBehaviour {
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start () {
+        //HinjiJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began && tag == "LeftFlipperTag":
+                    SetAngle(this.flickAngle);
+                    
+
+            }
+        }
+		
+	}
+}
